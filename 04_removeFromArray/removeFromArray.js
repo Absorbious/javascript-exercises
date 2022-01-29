b9b7cd386cfb5af5bfc6bd42e5b6ed60b06ffa33
Adnan Jusic <adnanjusic98@gmail.com>
2022-01-29 17:58:29 +0100

    Successfully tested exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let array = arr;
+  let restArgs = args;
+  let result = array.filter((param) => !restArgs.includes(param));
+  return result;
 
+  
 };
 
 // Do not edit below this line
