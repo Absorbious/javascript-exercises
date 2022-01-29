52a710abdb5e10fe0a8d2da83729246949d54701
Adnan Jusic <adnanjusic98@gmail.com>
2022-01-29 16:13:26 +0100

    Successfully tested exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+        input = string;
+        if(num == 0){
+            return "";
+        } else if(num > 0){
+            for(let i = 1; i < num; i++){
+                input+=string;
+            }
+            return input;
+        }else{
+            return 'ERROR';
+        }
+        
 };
 
 // Do not edit below this line
