762ea24b0c3682ad7b8690c172c221b26b7991db
Adnan Jusic <adnanjusic98@gmail.com>
2022-02-02 17:34:44 +0100

    Successfully tested exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const ftoc = function(degreeF) {
-return ((degreeF - 32)/1.8)
+  return Math.round(((degreeF - 32)/1.8) * 10) / 10;
 };
 
 const ctof = function(degreeC) {
-return ((degreeC * 1.8) + 32)
+  return Math.round(((degreeC * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
